64992e49aa3f621758ef5223150d14ba80b56517
Grakody <59213454+Faiznurullah@users.noreply.github.com>
2021-05-28 19:49:13 +0700

    add file

---
+++ Belajar Dart/type_data.dart
@@ -0,0 +1,22 @@
+// Ini Void Utama
+void main() {
+  
+ 
+  var Nama_Saya = "Dadang Kusnandar";
+  print(Nama_Saya.runtimeType);
+  // Output: String
+  
+  var Umur = 17;
+  print(Umur.runtimeType);
+  // Output: Int
+  
+  var Berat = 55.5;
+  print(Berat.runtimeType);
+  // Output: Double
+  
+  var Boleean = true;
+  print(Boleean.runtimeType);
+  // Output: Bolean
+  
+  
+}
